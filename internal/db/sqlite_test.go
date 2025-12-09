54660b68810005b3bf2c1896462f8a21ea9372da
genekkion <genekkion@live.com>
2025-12-09 22:47:40 +0800

    Add unit tests for hash storage functionality and SQLite test database helper

---
+++ internal/db/sqlite_test.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"sync/atomic"
+	"testing"
+
+	"build/internal/test"
+)
+
+var testCounter = atomic.Int32{}
+
+func newTestDb(t *testing.T) *sql.DB {
+	t.Helper()
+
+	count := testCounter.Add(1)
+	fp := fmt.Sprintf("test-%d.db", count)
+	fp = fmt.Sprintf("file:%s?mode=memory", fp)
+
+	db, err := New(fp)
+	test.NilErr(t, err)
+
+	return db
+}
